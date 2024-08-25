6770128bdf994b97601242b83c08e94d4a6c405f
sudhanshuJha01 <sudhanshujha883@gmail.com>
2024-08-26 00:24:17 +0530

    completed

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use('/api/v1' , rootRouter)
 
 
-app.listen(3000 , ()=>{
+app.listen(process.env.PORT || 3000 , ()=>{
     console.log("our app is running on the port 3000");
     
 })
